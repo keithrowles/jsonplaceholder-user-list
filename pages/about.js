ac6e3623be2af8da5688a8d846aa31b238befdcc
Keith Rowles <keithrowles@yahoo.com.au>
2022-10-26 14:24:55 +1000

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+        <title>Ninja List | About</title>
+        <meta name="description" content="This is the about us page for our ninja list." />
+        </Head>
+
+        <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat odio, tempora porro sit voluptatum voluptatibus illo quaerat nisi tempore, ea aliquid obcaecati hic. Debitis et quos nulla eligendi eaque voluptatibus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat odio, tempora porro sit voluptatum voluptatibus illo quaerat nisi tempore, ea aliquid obcaecati hic. Debitis et quos nulla eligendi eaque voluptatibus!</p>
+        </div>
+
+        </>
+
+     );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,31 @@
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
+
+
+// runs at build time
+export const getStaticProps = async () => {
+
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await res.json();
+
+        return {
+            props: { ninjas: data }
+        }
+}
+
+const Ninjas = ({ ninjas }) => {
+    return ( 
+        <div>
+            <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name} - {ninja.username}</h3>
+                    </a>
+                </Link>
+            ))}
+        </div>
+     );
+}
+ 
+export default Ninjas;
